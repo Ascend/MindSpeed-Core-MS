!51 move silent check all reduce to grad norm * change get silent check flag * silent check fix * add rms norm hook * move silent check all reduce to grad norm

ascendspeed/optimizer/optimizer.py
@@ -3,6 +3,31 @@ import os
 import torch
 
 
+def clear_silent_check():
+    if int(os.getenv('NPU_DETECT', '0')):
+        from torch_npu.utils.silent_error import clear_hookmodule_list
+        clear_hookmodule_list()
+
+
+def exec_silent_check(loss_scale):
+    if int(os.getenv('NPU_DETECT', '0')):
+        from torch_npu.utils.silent_error import silent_fault_check
+        silent_fault_check(loss_scale)
+
+
+def get_silent_check_flag():
+    if int(os.getenv('NPU_DETECT', '0')):
+        from torch_npu.utils.silent_error import get_silent_check
+        return get_silent_check()
+    return False
+
+
+def print_silent_check_log():
+    import torch_npu
+    if hasattr(torch_npu.npu, "print_error_plog"):
+        torch_npu.npu.print_error_plog("NPUCheckEvent:AICore Numerical error happen, skip this step!")
+
+
 @torch.no_grad()
 def mixed_precision_optimizer_step(self, args, timers):
     # Copy gradients from model params to main params.
@@ -27,10 +52,11 @@ def mixed_precision_optimizer_step(self, args, timers):
 
         # If we found inf/nan, skip the update.
         if found_inf_flag:
-            if int(os.getenv('NPU_DETECT', '0')):
-                from torch_npu.utils.silent_error import clear_hookmodule_list
-                clear_hookmodule_list()
+            clear_silent_check()
             return False, None, None
+
+    loss_scale = 1.0 if self.grad_scaler is None else self.grad_scaler.inv_scale.item()
+    exec_silent_check(loss_scale)
 
     # Clip the main gradients.
     timers('optimizer-clip-main-grad', log_level=1).start(
@@ -41,22 +67,13 @@ def mixed_precision_optimizer_step(self, args, timers):
                                         self.check_for_nan_in_grad)
     timers('optimizer-clip-main-grad').stop()
 
-    found_silent_flag = False
-    if int(os.getenv('NPU_DETECT', '0')):
-        from torch_npu.utils.silent_error import silent_fault_check
-        loss_scale = 1.0 if self.grad_scaler is None else self.grad_scaler.inv_scale.item()
-        silent_error = silent_fault_check(loss_scale)
-        silent_error = torch.tensor(silent_error, dtype=torch.float32).npu()
-        torch.distributed.all_reduce(silent_error, op=torch.distributed.ReduceOp.MAX)
-        found_silent_flag = (silent_error.item() > 0)
-
+    found_silent_flag = get_silent_check_flag()
     if not found_silent_flag or not (int(os.getenv('NPU_RECOVERY', '0'))):
-
         # Count the zeros in the grads.
         timers('optimizer-count-zeros', log_level=1).start(
             barrier=args.barrier_with_L1_time)
         num_zeros_in_grad = self.count_zeros() if \
-                            self.log_num_zeros_in_grad else None
+            self.log_num_zeros_in_grad else None
         timers('optimizer-count-zeros').stop()
 
         # Step the optimizer.
@@ -73,9 +90,7 @@ def mixed_precision_optimizer_step(self, args, timers):
 
     else:
         # The silent error is found, and skip the step, then call print_error_plog api to print log in plog.
-        import torch_npu
-        if hasattr(torch_npu.npu, "print_error_plog"):
-            torch_npu.npu.print_error_plog("NPUCheckEvent:AICore Numerical error happen, skip this step!")
+        print_silent_check_log()
         return False, None, None
 
     # Successful update.
@@ -97,6 +112,7 @@ def fp32_optimizer_step(self, args, timers):
 
     timers('optimizer-copy-to-main-grad').stop()
 
+    exec_silent_check(1.0)
     # Clip gradients.
     timers('optimizer-clip-main-grad', log_level=1).start(
         barrier=args.barrier_with_L1_time)
@@ -106,20 +122,13 @@ def fp32_optimizer_step(self, args, timers):
                                         self.check_for_nan_in_grad)
     timers('optimizer-clip-main-grad').stop()
 
-    found_silent_flag = False
-    if int(os.getenv('NPU_DETECT', '0')):
-        from torch_npu.utils.silent_error import silent_fault_check
-        silent_error = silent_fault_check(1.0)
-        silent_error = torch.tensor(silent_error, dtype=torch.float32).npu()
-        torch.distributed.all_reduce(silent_error, op=torch.distributed.ReduceOp.MAX)
-        found_silent_flag = (silent_error.item() > 0)
-
+    found_silent_flag = get_silent_check_flag()
     if not found_silent_flag or not (int(os.getenv('NPU_RECOVERY', '0'))):
         # count the zeros in the grads
         timers('optimizer-count-zeros', log_level=1).start(
             barrier=args.barrier_with_L1_time)
         num_zeros_in_grad = self.count_zeros() if \
-                            self.log_num_zeros_in_grad else None
+            self.log_num_zeros_in_grad else None
         timers('optimizer-count-zeros').stop()
 
         # Update parameters.
@@ -129,9 +138,7 @@ def fp32_optimizer_step(self, args, timers):
         timers('optimizer-inner-step').stop()
     else:
         # The silent error is found, and skip the step, then call print_error_plog api to print log in plog.
-        import torch_npu
-        if hasattr(torch_npu.npu, "print_error_plog"):
-            torch_npu.npu.print_error_plog("NPUCheckEvent:AICore Numerical error happen, skip this step!")
+        print_silent_check_log()
         return False, None, None
 
     # No overflow for FP32 optimizer.