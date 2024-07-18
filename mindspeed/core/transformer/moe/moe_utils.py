5bf0aab042422500d8c722189bfefa01266690d4
jializheng <jializheng@huawei.com>
2024-07-18 20:45:22 +0800

    moe unpermute optim

--- mindspeed/core/transformer/moe/moe_utils.py
+++ mindspeed/core/transformer/moe/moe_utils.py
@@ -11,3 +11,35 @@ def permute(tokens, indices, topk: int = 1):
     sorted_indices = torch.sort(flatten_indices.float(), stable=True)[1]
     permuted_tokens = tokens.index_select(0, sorted_indices // topk)
     return permuted_tokens, sorted_indices
+
+
+def unpermute(
+    permuted_tokens: torch.Tensor,
+    sorted_indices: torch.Tensor,
+    probs: torch.Tensor = None,
+    padded_mode: bool = False,
+    restore_shape: torch.Size = None,
+):
+    if padded_mode:
+        return unpermute_with_padded_tokens(
+            permuted_tokens, sorted_indices, probs, restore_shape=restore_shape
+        )
+
+    assert sorted_indices.numel() == permuted_tokens.size(0)
+    if probs is not None:
+        # Unpermute and merge the tokens with their probabilities
+        num_unpermuted_tokens = probs.numel()
+        topk = probs.size(1)
+    else:
+        # Unpermute the tokens without merge
+        num_unpermuted_tokens = permuted_tokens.size(0)
+        topk = 1
+
+    sorted_indices = torch.argsort(sorted_indices.float()).int()
+    unpermuted_tokens = permuted_tokens.index_select(0, sorted_indices)
+    unpermuted_tokens = unpermuted_tokens.reshape(-1, topk, permuted_tokens.size(-1))
+    if probs is not None:
+        unpermuted_tokens = unpermuted_tokens * probs.unsqueeze(-1)
+    unpermuted_tokens = unpermuted_tokens.sum(dim=1)
+
+    return unpermuted_tokens
