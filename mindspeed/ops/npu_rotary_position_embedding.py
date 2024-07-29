ccf587e07ed84a09ea74af1049de39d88962bdf8
rui_everAfter <wangmingrui3@huawei.com>
2024-07-29 08:50:21 +0000

    !589 将OpBuilder load操作移到方法中 Merge pull request !589 from rui_everAfter/master

--- mindspeed/ops/npu_rotary_position_embedding.py
+++ mindspeed/ops/npu_rotary_position_embedding.py
@@ -1,7 +1,8 @@
 import torch
 from mindspeed.op_builder import RotaryPositionEmbeddingOpBuilder
-rope_ops = RotaryPositionEmbeddingOpBuilder().load()
+rope_op_builder = RotaryPositionEmbeddingOpBuilder()
 
 
 def npu_rotary_position_embedding(x, cos, sin, mode=0):
+    rope_ops = rope_op_builder.load()
     return rope_ops.npu_rotary_position_embedding(x, cos, sin, mode)
