83a95ab7035dbb72199b8abe51d477892c6fde9b
mwang <wangmin106@huawei.com>
2024-09-25 12:34:15 +0800

    init mindspeed core ms

--- mindspeed/ops/npu_rotary_position_embedding.py
+++
@@ -1,8 +0,0 @@
-import torch
-from mindspeed.op_builder import RotaryPositionEmbeddingOpBuilder
-rope_op_builder = RotaryPositionEmbeddingOpBuilder()
-
-
-def npu_rotary_position_embedding(x, cos, sin, mode=0):
-    rope_ops = rope_op_builder.load()
-    return rope_ops.npu_rotary_position_embedding(x, cos, sin, mode)
