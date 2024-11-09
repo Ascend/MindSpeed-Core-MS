028c53c013d4ba6770eeca5e9513816e34c95fe0
kong_de_shu <1670690897@qq.com>
2024-11-09 22:18:29 +0800

    add_ulysses_st

--- mindspeed_ms/core/context_parallel/ulysses_context_parallel.py
+++ mindspeed_ms/core/context_parallel/ulysses_context_parallel.py
@@ -1,8 +1,18 @@
-# coding=utf-8
-# Copyright (c) 2024, Huawei Technologies Co., Ltd. All rights reserved.
-# Copyright (c) Microsoft Corporation.
-# SPDX-License-Identifier: Apache-2.0
-
+# Copyright 2024 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Ulysses Context Parallel APIs."""
 from typing import Any
 import mindspore
 from mindspore import Tensor, mint
@@ -12,6 +22,26 @@ from mindspore.communication.comm_func import all_to_all_single_with_output_shap
 
 
 def single_all_to_all(input_, scatter_idx, gather_idx, group):
+    """
+    Perform an all-to-all communication operation on a distributed tensor.
+
+    This function reshapes the input tensor, performs an all-to-all operation across the specified communication group,
+    and reshapes the output back to its original format with a modified dimension.
+
+    Args:
+        input_ (Tensor): The input tensor to be scattered and gathered.
+        scatter_idx (int): The index of the dimension to scatter.
+        gather_idx (int): The index of the dimension to gather.
+        group: The communication group within which the all-to-all operation will be performed.
+
+    Returns:
+        Tensor: The reshaped output tensor after the all-to-all operation.
+
+    Notes:
+        - If `scatter_idx` < 2, the tensor is reshaped and scattered along the sequence dimension.
+        - Otherwise, tensor heads are transposed for parallel computation.
+        - The output is then gathered and reshaped to match the original dimensions.
+    """
     seq_world_size = get_world_size(group)
     inp_shape = list(input_.shape)
     inp_shape[scatter_idx] = inp_shape[scatter_idx] // seq_world_size
@@ -28,16 +58,37 @@ def single_all_to_all(input_, scatter_idx, gather_idx, group):
         ).swapaxes(0, 1).contiguous()
     output = mint.zeros_like(input_t)
     output = all_to_all_single_with_output_shape(output.shape, input_t, group=group)
+    output = output[0]
     # if scattering the seq-dim, transpose the heads back to the original dimension
     # e.g., [cp, s/cp, b, n/cp, d] -> [s/cp, b, cp, n/cp, d]
     if scatter_idx < 2:
         output = output.swapaxes(0, 1).swapaxes(1, 2).contiguous()
 
     return output.reshape(
-        inp_shape[: gather_idx] + [inp_shape[gather_idx] * seq_world_size, ] + inp_shape[gather_idx + 1:]).contiguous()
+        inp_shape[:gather_idx] + [inp_shape[gather_idx] * seq_world_size] + inp_shape[gather_idx + 1:]
+    ).contiguous()
 
 
 class _SeqAllToAll(mindspore.nn.Cell):
+    """
+        A MindSpore neural network cell for performing sequence-parallel all-to-all communication.
+
+        This class wraps the `single_all_to_all` function, enabling sequence-parallel operations
+        as a neural network layer. It supports both forward and backward computations
+        for gradient propagation in a distributed setting.
+
+        Args:
+            group: The communication group within which the all-to-all operation is performed.
+            scatter_idx (int): The index of the dimension to scatter in the input tensor.
+            gather_idx (int): The index of the dimension to gather in the output tensor.
+
+        Methods:
+            construct(input_):
+                Forward computation for the sequence-parallel all-to-all operation.
+            bprop(*args):
+                Backward computation for gradient propagation, performing an all-to-all
+                operation on the gradient tensor.
+        """
     def __init__(self, group, scatter_idx: int, gather_idx: int):
         super().__init__()
         self.group = group
