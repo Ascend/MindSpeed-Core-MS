5424a3447b37dae3a6e3c15a92bfbee3e39ced61
Dring <17737727613@163.com>
2025-01-19 03:11:13 +0000

    update apply_mm.sh.
    Signed-off-by: Dring <17737727613@163.com>

--- apply_mm.sh
+++ apply_mm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "===========================================start apply==========================================="
+rm -rf MindSpeed-MM/
 git clone https://gitee.com/ascend/MindSpeed-MM.git
 cd MindSpeed-MM/
 git checkout 9526e82399d9db8a18cf3f2aa7089f0b928e57b0
@@ -8,6 +9,7 @@ git apply ../msadaptor/mindspeed_mm.diff
 cd ..
 echo "..............................................done apply mindspeed_mm"
 
+rm -rf MindSpeed/
 git clone https://gitee.com/ascend/MindSpeed.git
 cd MindSpeed
 git checkout ab39de78be23e88e2c8b0d25edf6135940990c02
@@ -15,6 +17,7 @@ git apply ../msadaptor/acclerate_mindspeed_mm.diff
 cd ..
 echo "...............................................done apply acclerate_mindspeed_mm"
 
+rm -rf Megatron-LM/
 git clone https://github.com/NVIDIA/Megatron-LM.git
 cd Megatron-LM/
 git checkout core_r0.6.0
@@ -22,6 +25,7 @@ git apply ../msadaptor/megatron.diff
 cd ..
 echo "..............................................done apply megatron"
 
+rm -rf transformers/
 pip install transformers==4.47.0
 mkdir src
 TF_PATH=$(python -c "import site; print(site.getsitepackages()[0])")
@@ -35,5 +39,7 @@ mv src/ transformers/
 echo "..............................................done apply transformers"
 
 pip install -r requirements.txt
+echo "..............................................done install requirements"
+
 echo "=======================================finish apply======================================="
 
