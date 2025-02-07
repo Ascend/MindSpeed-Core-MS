c08bd8359a348e3ace853b45acdc894cc9fded7d
孔德硕 <1670690897@qq.com>
2025-02-07 18:12:36 +0800

    llava1.5 support

--- msadaptor/torch/__init__.py
+++ msadaptor/torch/__init__.py
@@ -15,6 +15,8 @@
 """core module"""
 import os
 import platform
+import sys
+import torch
 from packaging import version
 import mindspore
 from mindspore import context
@@ -120,3 +122,24 @@ def compile(fn=None, *args, **kwargs):
     return wrap_func
 
 __version__ = "2.5"
+
+
+def _register_device_module(device_type, module):
+    r"""Register an external runtime module of the specific :attr:`device_type`
+    supported by torch.
+    After the :attr:`module` is registered correctly, the user can refer
+    the external runtime module as part of torch with attribute torch.xxx.
+    """
+    # Make sure the device_type represent a supported device type for torch.
+    # device_type = torch.device(device_type)
+    m = sys.modules[__name__]
+    if hasattr(m, device_type):
+        raise RuntimeError(f"The runtime module of '{device_type}' has already "
+                           f"been registered with '{getattr(m, device_type)}'")
+    setattr(m, device_type, module)
+    torch_module_name = '.'.join([__name__, device_type])
+    sys.modules[torch_module_name] = module
+
+
+def where(condition):
+    return condition.nonzero(as_tuple=True)
