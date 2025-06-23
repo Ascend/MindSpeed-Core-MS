eaa89fbec5a378f79b52a7b891e236b054e3034c
Albert <albert.zhang.hit@gmail.com>
2025-06-23 10:43:59 +0800

    fix shell

--- auto_convert_llm.sh
+++ auto_convert_llm.sh
@@ -32,7 +32,7 @@ echo "..............................................done Megatron-LM"
 
 #msadapter
 rm -rf MSAdapter
-git clone https://openi.pcl.ac.cn/OpenI/MSAdapter.git -b r0.2.0
+git clone https://openi.pcl.ac.cn/OpenI/MSAdapter.git -b master
 if [ $? -ne 0 ]; then
     echo "Error: git clone MSAdapter"
     exit 1
@@ -68,6 +68,40 @@ git apply ../tools/rules/accelerate.diff
 rm -rf tests
 cd ..
 echo "..............................................done apply accelerate"
+
+#safetensors
+rm -rf safetensors_dir
+mkdir safetensors_dir
+pip install --no-deps safetensors==0.5.1
+if [ $? -ne 0 ]; then
+    echo "Error: pip install safetensors fail"
+else
+    ST_PATH=$(python -c "import site; print(site.getsitepackages()[0])")
+    cp -r ${ST_PATH}/safetensors ./safetensors_dir
+    cd safetensors_dir/safetensors
+    git init
+    git apply ../../tools/rules/safetensors.diff
+    cd ../../
+    export PYTHONPATH=$(pwd)/safetensors_dir:$PYTHONPATH
+    echo "..............................................done apply safetensors"
+fi
+
+#huggingface_hub
+rm -rf huggingface_hub_dir
+mkdir huggingface_hub_dir
+pip install --no-deps huggingface_hub==0.32.3
+if [ $? -ne 0 ]; then
+    echo "Error: pip install huggingface_hub fail"
+else
+    ST_PATH=$(python -c "import site; print(site.getsitepackages()[0])")
+    cp -r ${ST_PATH}/huggingface_hub ./huggingface_hub_dir
+    cd huggingface_hub_dir/huggingface_hub
+    git init
+    git apply ../../tools/rules/huggingface_hub.diff
+    cd ../../
+    export PYTHONPATH=$(pwd)/huggingface_hub_dir:$PYTHONPATH
+    echo "..............................................done apply huggingface_hub"
+fi
 
 echo "..............................................start code_convert"
 MindSpeed_Core_MS_PATH=$(pwd)
