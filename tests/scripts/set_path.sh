4e25ace5815e60285475391b1a89c2bf1dc5b7af
y30062407 <handsome0324@163.com>
2025-06-24 15:15:13 +0800

    [update][master]update shell about set PYTHONPATH

--- tests/scripts/set_path.sh
+++ tests/scripts/set_path.sh
@@ -6,6 +6,8 @@ script_path=$(realpath "${BASH_SOURCE[0]}")
 script_dir=$(dirname "$script_path")
 parent_dir=$(dirname "$script_dir")
 MindSpeed_Core_MS_PATH=$(dirname "$parent_dir")
+export PYTHONPATH=${MindSpeed_Core_MS_PATH}/safetensors_dir:$PYTHONPATH
+export PYTHONPATH=${MindSpeed_Core_MS_PATH}/huggingface_hub_dir:$PYTHONPATH
 export PYTHONPATH=${MindSpeed_Core_MS_PATH}/msadapter/mindtorch/:$PYTHONPATH
 export PYTHONPATH=${MindSpeed_Core_MS_PATH}/MindSpeed-LLM/:${MindSpeed_Core_MS_PATH}/Megatron-LM/:${MindSpeed_Core_MS_PATH}/MindSpeed/:${MindSpeed_Core_MS_PATH}/transformers/src/:${MindSpeed_Core_MS_PATH}/accelerate/src/:$PYTHONPATH
 echo "..............................................done set PYTHONPATH"
