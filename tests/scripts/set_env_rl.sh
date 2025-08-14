8b3975441de29dc126f5dc1e0da6ffccc276e3b0
wangyibo <1605891897@qq.com>
2025-08-14 11:17:38 +0800

    update tests/scripts

--- tests/scripts/set_env_rl.sh
+++ tests/scripts/set_env_rl.sh
@@ -7,5 +7,8 @@ mkdir ${MindSpeed_Core_MS_PATH}/RL
 cp -r ${MindSpeed_Core_MS_PATH}/test_convert_rl.sh ${MindSpeed_Core_MS_PATH}/RL
 cp -r ${MindSpeed_Core_MS_PATH}/tools ${MindSpeed_Core_MS_PATH}/RL
 cd RL
+sed -i '/#safetensors/,/#huggingface_hub/ s/^/#/' test_convert_rl.sh
+sed -i '/#huggingface_hub/,/echo "..............................................start code_convert"/ s/^/#/' test_convert_rl.sh
+sed -i '/echo "..............................................start code_convert"/ s/^#//' test_convert_rl.sh
 bash test_convert_rl.sh is_rl_gongka
 echo "..............................................done set RL_env"
