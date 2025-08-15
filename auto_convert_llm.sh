d560a80db0bfef18b8ee206cadf2e05c7352fcba
周徐敏 <m19825086558@163.com>
2025-08-15 16:53:18 +0800

    update PYTHONPATH

--- auto_convert_llm.sh
+++ auto_convert_llm.sh
@@ -56,55 +56,6 @@ rm -rf tests
 cd ..
 echo "..............................................done apply transformers"
 
-#accelerate
-rm -rf accelerate/
-git clone https://gitee.com/modelee/accelerate.git -b v1.6.0 --depth 1
-if [ $? -ne 0 ]; then
-    echo "Error: git clone accelerate"
-    exit 1
-fi
-cd accelerate
-git apply ../tools/rules/accelerate.diff
-rm -rf tests
-cd ..
-echo "..............................................done apply accelerate"
-
-#safetensors
-rm -rf safetensors_dir
-mkdir safetensors_dir
-pip uninstall safetensors -y 
-pip install --no-deps safetensors==0.5.1
-if [ $? -ne 0 ]; then
-    echo "Error: pip install safetensors fail"
-else
-    ST_PATH=$(python -c "import site; print(site.getsitepackages()[0])")
-    cp -r ${ST_PATH}/safetensors ./safetensors_dir
-    cd safetensors_dir/safetensors
-    git init
-    git apply ../../tools/rules/safetensors.diff
-    cd ../../
-    export PYTHONPATH=$(pwd)/safetensors_dir:$PYTHONPATH
-    echo "..............................................done apply safetensors"
-fi
-
-#huggingface_hub
-rm -rf huggingface_hub_dir
-mkdir huggingface_hub_dir
-pip uninstall huggingface_hub -y
-pip install --no-deps huggingface_hub==0.32.3
-if [ $? -ne 0 ]; then
-    echo "Error: pip install huggingface_hub fail"
-else
-    ST_PATH=$(python -c "import site; print(site.getsitepackages()[0])")
-    cp -r ${ST_PATH}/huggingface_hub ./huggingface_hub_dir
-    cd huggingface_hub_dir/huggingface_hub
-    git init
-    git apply ../../tools/rules/huggingface_hub.diff
-    cd ../../
-    export PYTHONPATH=$(pwd)/huggingface_hub_dir:$PYTHONPATH
-    echo "..............................................done apply huggingface_hub"
-fi
-
 echo "..............................................start code_convert"
 MindSpeed_Core_MS_PATH=$(pwd)
 echo ${MindSpeed_Core_MS_PATH}
@@ -114,7 +65,7 @@ python3 tools/transfer.py \
 --mindspeed_path ${MindSpeed_Core_MS_PATH}/MindSpeed/mindspeed/ \
 --mindspeed_llm_path ${MindSpeed_Core_MS_PATH}/MindSpeed-LLM/
 
-export PYTHONPATH=${MindSpeed_Core_MS_PATH}/MSAdapter/mindtorch:${MindSpeed_Core_MS_PATH}/Megatron-LM:${MindSpeed_Core_MS_PATH}/MindSpeed:${MindSpeed_Core_MS_PATH}/MindSpeed-LLM:${MindSpeed_Core_MS_PATH}/transformers/src/:${MindSpeed_Core_MS_PATH}/accelerate/src/:$PYTHONPATH
+export PYTHONPATH=${MindSpeed_Core_MS_PATH}/MSAdapter/mindtorch:${MindSpeed_Core_MS_PATH}/MindSpeed-LLM:${MindSpeed_Core_MS_PATH}/Megatron-LM:${MindSpeed_Core_MS_PATH}/MindSpeed:${MindSpeed_Core_MS_PATH}/transformers/src/:$PYTHONPATH
 echo $PYTHONPATH
 echo "..............................................done code_convert"
 
--- tests/scripts/set_path.sh
+++ tests/scripts/set_path.sh
@@ -6,10 +6,8 @@ script_path=$(realpath "${BASH_SOURCE[0]}")
 script_dir=$(dirname "$script_path")
 parent_dir=$(dirname "$script_dir")
 MindSpeed_Core_MS_PATH=$(dirname "$parent_dir")
-export PYTHONPATH=${MindSpeed_Core_MS_PATH}/safetensors_dir:$PYTHONPATH
-export PYTHONPATH=${MindSpeed_Core_MS_PATH}/huggingface_hub_dir:$PYTHONPATH
 export PYTHONPATH=${MindSpeed_Core_MS_PATH}/msadapter/:${MindSpeed_Core_MS_PATH}/MSAdapter/:$PYTHONPATH
 export PYTHONPATH=${MindSpeed_Core_MS_PATH}/msadapter/msa_thirdparty/:${MindSpeed_Core_MS_PATH}/MSAdapter/msa_thirdparty:$PYTHONPATH
-export PYTHONPATH=${MindSpeed_Core_MS_PATH}/MindSpeed-LLM/:${MindSpeed_Core_MS_PATH}/Megatron-LM/:${MindSpeed_Core_MS_PATH}/MindSpeed/:${MindSpeed_Core_MS_PATH}/transformers/src/:${MindSpeed_Core_MS_PATH}/accelerate/src/:$PYTHONPATH
+export PYTHONPATH=${MindSpeed_Core_MS_PATH}/MindSpeed-LLM/:${MindSpeed_Core_MS_PATH}/Megatron-LM/:${MindSpeed_Core_MS_PATH}/MindSpeed/:${MindSpeed_Core_MS_PATH}/transformers/src/:$PYTHONPATH
 echo "..............................................done set PYTHONPATH"
 echo $PYTHONPATH
