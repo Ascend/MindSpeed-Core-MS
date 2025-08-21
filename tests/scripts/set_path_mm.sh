8ecf81340ed2c3d5164638a37c4fbad07e0ae7e6
sl_wang <wangshuangling1@huawei.com>
2025-08-21 09:50:44 +0800

    remove third_party diff files for mindspeed-mm

--- tests/scripts/set_path_mm.sh
+++ tests/scripts/set_path_mm.sh
@@ -7,7 +7,6 @@ script_dir=$(dirname "$script_path")
 parent_dir=$(dirname "$script_dir")
 MindSpeed_Core_MS_PATH=$(dirname "$parent_dir")/MM
 export PYTHONPATH=${MindSpeed_Core_MS_PATH}/msadapter/:${MindSpeed_Core_MS_PATH}/msadapter/msa_thirdparty/:$PYTHONPATH
-export PYTHONPATH=${MindSpeed_Core_MS_PATH}/safetensors_dir/:$PYTHONPATH
-export PYTHONPATH=${MindSpeed_Core_MS_PATH}/MindSpeed-MM/:${MindSpeed_Core_MS_PATH}/Megatron-LM/:${MindSpeed_Core_MS_PATH}/MindSpeed/:${MindSpeed_Core_MS_PATH}/transformers/src/:$PYTHONPATH
+export PYTHONPATH=${MindSpeed_Core_MS_PATH}/MindSpeed-MM/:${MindSpeed_Core_MS_PATH}/Megatron-LM/:${MindSpeed_Core_MS_PATH}/MindSpeed/:$PYTHONPATH
 echo "..............................................done set PYTHONPATH"
 echo $PYTHONPATH
