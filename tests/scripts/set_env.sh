df7a332ff13ff1267d6bcbbca02148a27753fd84
wangyibo <1605891897@qq.com>
2025-08-22 10:06:49 +0800

    bugfix

--- tests/scripts/set_env.sh
+++ tests/scripts/set_env.sh
@@ -4,6 +4,17 @@ script_dir=$(dirname "$script_path")
 parent_dir=$(dirname "$script_dir")
 MindSpeed_Core_MS_PATH=$(dirname "$parent_dir")
 cd ${MindSpeed_Core_MS_PATH}
+script_file="test_convert_llm.sh"
+sed -i '/rm -rf MindSpeed-LLM\/tests/i \
+cd MindSpeed-LLM\n\
+git checkout 7127be594109d796799edb5c1906e68a926fb536\n\
+cd ..' "$script_file"
+if [ $? -eq 0 ]; then
+    echo "MindSpeed-LLM git checkout Successfully: $script_file"
+else
+    echo "Error: MindSpeed-LLM git checkout fail: $script_file"
+    exit 1
+fi
 bash test_convert_llm.sh
 modifygrammar() {
     fname=$1
