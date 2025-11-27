384b62af4b0fb8fcd4550d233790e7b40611d435
ybwang19 <1605891897@qq.com>
2025-11-27 10:20:17 +0800

    bugfix CI env

--- tests/scripts/set_env.sh
+++ tests/scripts/set_env.sh
@@ -6,7 +6,7 @@ MindSpeed_Core_MS_PATH=$(dirname "$parent_dir")
 cd ${MindSpeed_Core_MS_PATH}
 sed -i '/echo ${MindSpeed_Core_MS_PATH}/i rm -rf MindSpeed-LLM\/tests\nrm -rf MindSpeed\/tests_extend\nrm -rf Megatron-LM\/tests\nrm -rf msadapter\/tests' auto_convert.sh llm
 bash auto_convert.sh llm msa_latest
-modifygrammar() {
+modifygrammar1() {
     fname=$1
     echo "Modifying PY310 grammar to adapt PY39..."
     # replace num_query_groups: int | None = None
@@ -15,5 +15,15 @@ modifygrammar() {
     sed -i 's/^[[:space:]]*ffn_hidden_size:.*= None/    ffn_hidden_size = None/' "$fname"
     echo "PY310 grammar have been updated to adapt PY39 in $fname"
 }
-modifygrammar ${MindSpeed_Core_MS_PATH}/Megatron-LM/megatron/core/transformer/heterogeneous/heterogeneous_config.py
+modifygrammar2() {
+    fname=$1
+    echo "Modifying PY310 grammar to adapt PY39..."
+    # replace from typing import Type with from typing import Union, Type
+    sed -i 's/^from typing import Type/from typing import Union, Type/' "$fname"
+    # replace Type['BaseGMMFunction'] | None with Union[Type['BaseGMMFunction'], None]
+    sed -i "s/-> Type\['BaseGMMFunction'\] | None:/-> Union[Type['BaseGMMFunction'], None]:/" "$fname"
+    echo "PY310 grammar have been updated to adapt PY39 in $fname"
+}
+modifygrammar1 ${MindSpeed_Core_MS_PATH}/Megatron-LM/megatron/core/transformer/heterogeneous/heterogeneous_config.py
+modifygrammar2 ${MindSpeed_Core_MS_PATH}/MindSpeed/mindspeed/core/transformer/moe/grouped_matmul_util.py
 echo "..............................................done set LLM_env"
